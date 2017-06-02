604dd9988c79e438821d20f2f4afe3ce9ed25e0f
Priscilla Cheng <priscillaccheng@gmail.com>
2017-06-02 00:22:40 -0700

    first commit

---
+++ lab3b.py
@@ -0,0 +1,15 @@
+import csv
+
+	
+
+if __name__=="__main__":
+    #-----------open file----------------------
+    with open('trivial.csv', newline="") as trivial_csv:
+    	trivialfs = csv.reader(trivial_csv, delimiter=',')
+    	
+    for row in trivialfs:
+    	print(row)
+    	
+    
+
+
