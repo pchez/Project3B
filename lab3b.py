031cacc4ca47453b32c6f100045cf7d95eb020fd
Saurabh <saurabhdeo27@gmail.com>
2017-06-05 15:32:40 -0700

    FIXED error that 0 is not invalid block for inode i_block array check

--- lab3b.py
+++ lab3b.py
@@ -80,11 +80,11 @@ def blockConsistencyHelper(inode, superblock, group):
 				else:
 					blockType = ''
 					
-				if int(inode[key][j]) <= 0 or int(inode[key][j]) > int(superblock[0]): #invalid block
+				if int(inode[key][j]) < 0 or int(inode[key][j]) > int(superblock[0]): #invalid block
 					print('INVALID ', blockType, 'BLOCK ', inode[key][j], ' IN INODE ', key, ' AT OFFSET ', j-10, sep="")  
 				
-				elif int(inode[key][j]) < lastInodeBlk : #reserved block
-					print('RESERVED ', blockType, 'BLOCK ', inode[key][j], ' IN INODE ', key, ' AT OFFSET', j-10, sep="")
+				elif int(inode[key][j]) < lastInodeBlk and int(inode[key][j]) > 0: #reserved block
+					print('RESERVED ', blockType, 'BLOCK ', inode[key][j], ' IN INODE ', key, ' AT OFFSET ', j-10, sep="")
 				
 				else: #mark block as visited or duplicate (maybe create a new array with markers per block) 
 				 	if inode[key][j] in referenced:
@@ -92,8 +92,8 @@ def blockConsistencyHelper(inode, superblock, group):
 				 	else:
 				 		referenced.append(int(inode[key][j]))
 				 		
-		else: #inode block itself has error -- how to handle???
-			print('INVALID BLOCK ', inode[key], ' IN INODE ', inode[key], ' AT OFFSET ???', sep="")  
+		#else: #inode block itself has error -- how to handle???
+			#print('INVALID BLOCK ', inode[key], ' IN INODE ', inode[key], ' AT OFFSET ???', sep="")  
 	
 
 	for key in indirect.keys():
